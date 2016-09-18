df3d4d9af33df76fa57875bfcbd4cf5802f66aaa
sdebdas007 <sdebdas43@gmail.com>
2016-09-18 23:15:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/article-one', function(req,res){
     res.send("Article one is requested and we served here")
 })
 app.get('/article-two', function(req,res){
-    res.send("Article two is requested and we served here")
+    res.sendFile(path.join(__dirname, 'ui', 'airticle-one.html'));
 })
 app.get('/article-three', function(req,res){
     res.send("Article three is requested and we served here")
